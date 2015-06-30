604c422b8e0352d6b176470f111883936d5bf4fc
David Richied <DavidRichied@gmail.com>
2015-06-30 15:22:01 -0400

    Customized spoiler app

--- js/spoiler_app.js
+++ js/spoiler_app.js
@@ -1,15 +1,83 @@
 // Problem: Prevent spoilerphobes from seeing spoilers
 //Soluction: Hide spoilers and reveal them through user interaction
 
-//1. Hide spoiler
+//1. Hide spoilers.
+//2. Add a button to show spoilers
+
+
+
+
 $(".spoiler span").hide();
-//2. Add a button
-$(".spoiler").append("<button>Reveal Spoiler!</button>")
-//3. When button is pressed
+
+$(".spoiler").after("<button>Show Spoilers!</button>");
+
 $("button").click(function() {
-  //3.1 Show spoiler next to the button clicked
-  $(this).prev().show();
-//  $(".spoiler span").show();
-  //3.2 Get rid of button
-  $(this).remove();
+    var name = $(this).prev().children().css("display");
+    console.log(name);
+    if(name === "none") {
+        $(this).prev().children().toggle();
+        $(this).html("Hide Spoilers!")
+    } else {
+        if(name === "inline-block") {
+            $(this).prev().children().toggle();
+            $(this).html("Show Spoilers!")
+        }
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////1. Hide spoiler
+//$(".spoiler span").hide();
+////2. Add a button
+//$(".spoiler").append("<button>Reveal Spoiler!</button>")
+////3. When button is pressed
+//$("button").click(function() {
+//  //3.1 Show spoiler next to the button clicked
+//  $(this).prev().show();
+////  $(".spoiler span").show();
+//  //3.2 Get rid of button
+//  $(this).remove();
+//});
